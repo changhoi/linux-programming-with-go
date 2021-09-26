e644284754c007fdd2eaaee5dbed1e8ed7f146e8
cadenzah <cadenzah93@gmail.com>
2021-09-26 15:37:10 +0900

    파싱 로직의 별도 함수화

--- practice/html-parse/main.go
+++ practice/html-parse/main.go
@@ -10,19 +10,14 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-// a 태그 통하여 사용된 것들을 쭉 모아서
-// role, class 등을 표시하고
-// 하위의 태그들을 정리
-// 정리하여 엑셀로 뽑기 -> github.com/xuri/excelize/v2
+// 파싱 결과를 담는 구조체
 type Result struct {
-	Url                string
-	Class              string
-	Role               string
-	HasChildren        bool
-	ImmediateChildType string
+	Url                string // 링크 주소
+	Class              string // HTML Class Attribute value
+	Role               string // HTML Role value
+	ImmediateChildType string // 자식 요소가 있을 경우, 해당 요소의 태그
+	Text               string // a 태그의 텍스트 값
 }
-
-var ResultList []Result
 
 // 인자로 받은 행/열 정보 바탕으로
 // 엑셀 형식에 맞게 새로운 셀을 생성하여 시트에 추가
@@ -35,10 +30,58 @@ func SetRowValues(file *excelize.File, sheet string, rowNumBase int, values []st
 	}
 }
 
-//
+// 머리행의 스타일 설정 - 배경 설정은 작동하지 않음
+func SetHeaderRowStyle(file *excelize.File, sheet string) {
+	style, err := file.NewStyle(`{
+		"fill": {
+			"type": "pattern",
+			"color": ["#E0EBF5"],
+			"pattern": 1
+		}
+	}`)
+	if err != nil {
+		file.SetCellStyle(sheet, "A1", "E1", style)
+	}
+	file.SetPanes(sheet, `{
+		"freeze": true,
+		"split": false,
+		"x_split": 0,
+		"y_split": 1,
+		"top_left_cell": "A2"
+	}`)
+}
+
+// 'a' 태그에 해당하는 요소를 파싱하고, URL 관련 정보를 파싱하여 반환
+func Parse(document *goquery.Document) []Result {
+	var results []Result
+
+	document.Find("a").Each(func(i int, element *goquery.Selection) {
+		immediateChildType := ""
+
+		url := element.AttrOr("href", "")
+		className := element.AttrOr("class", "")
+		role := element.AttrOr("role", "")
+		text := element.Text()
+
+		if element.Children().Length() > 0 {
+			element = element.Children()
+			immediateChildType = goquery.NodeName(element)
+		}
+
+		results = append(results, Result{
+			Url:                url,
+			Class:              className,
+			Role:               role,
+			ImmediateChildType: immediateChildType,
+			Text:               text,
+		})
+	})
+
+	return results
+}
 
 func main() {
-	resp, err := http.Get("https://naver.com")
+	resp, err := http.Get("https://velog.io")
 	if err != nil {
 		panic(err)
 	}
@@ -52,46 +95,13 @@ func main() {
 	SetRowValues(file, sheetName, 1, []string{"URL", "Class", "Role", "하위 요소", "텍스트"})
 
 	// 머리행 스타일 설정
-	style, err := file.NewStyle(`{
-		"fill": {
-			"type": "pattern",
-			"color": ["#E0EBF5"],
-			"pattern": 1
-		}
-	}`)
-	if err != nil {
-		file.SetCellStyle(sheetName, "A1", "A1", style)
-		file.SetCellStyle(sheetName, "B1", "B1", style)
-		file.SetCellStyle(sheetName, "C1", "C1", style)
-		file.SetCellStyle(sheetName, "D1", "D1", style)
-		file.SetCellStyle(sheetName, "E1", "E1", style)
-	}
-	file.SetPanes(sheetName, `{
-		"freeze": true,
-		"split": false,
-		"x_split": 0,
-		"y_split": 1,
-		"top_left_cell": "A2"
-	}`)
-
-	doc.Find("a").Each(func(i int, element *goquery.Selection) {
-		index := i + 2
-		immediateChildType := ""
-
-		url := element.AttrOr("href", "")
-		className := element.AttrOr("class", "")
-		role := element.AttrOr("role", "")
-		text := element.Text()
-
-		if element.Children().Length() > 0 {
-			element = element.Children()
-			immediateChildType = goquery.NodeName(element)
-		}
-
-		// excel로 뽑아내기
-		SetRowValues(file, sheetName, index, []string{url, className, role, immediateChildType, text})
-	})
+	SetHeaderRowStyle(file, sheetName)
 
 	// excel로 저장
+	parsedData := Parse(doc)
+	for i, result := range parsedData {
+		index := i + 2
+		SetRowValues(file, sheetName, index, []string{result.Url, result.Class, result.Role, result.ImmediateChildType, result.Text})
+	}
 	file.SaveAs("parse-result.xlsx")
 }
